0bbd2748b8909d7d5dca39ee82ba5d4b5abc8083
TED Vortex <ted.vortex@gmail.com>
2021-08-08 20:00:02 +0200

    fix: update manually modified resources

--- vortexdotname.tf
+++ vortexdotname.tf
@@ -87,6 +87,15 @@ resource "cloudflare_record" "vortexdotname_CNAME_dotfiles_vortex_name" {
   value   = "0-vortex.github.io"
 }
 
+resource "cloudflare_record" "vortexdotname_CNAME_open-sauced-explorer-test_vortex_name" {
+  zone_id = data.cloudflare_zones.vortexdotname.zones[0].id
+  name    = "open-sauced-explorer-test"
+  type    = "CNAME"
+  ttl     = "1"
+  proxied = "true"
+  value   = "0-vortex.github.io"
+}
+
 resource "cloudflare_record" "vortexdotname_CNAME_tedvortex-xyz_vortex_name" {
   zone_id = data.cloudflare_zones.vortexdotname.zones[0].id
   name    = "tedvortex-xyz"
@@ -112,4 +121,13 @@ resource "cloudflare_record" "vortexdotname_CNAME_dark-elise_vortex_name" {
   ttl     = "1"
   proxied = "true"
   value   = "0-vortex.github.io"
+}
+
+resource "cloudflare_record" "vortexdotname_CNAME_google-site-verification_vortex_name" {
+  zone_id = data.cloudflare_zones.vortexdotname.zones[0].id
+  name    = "vortex.name"
+  type    = "CNAME"
+  ttl     = "1"
+  proxied = "false"
+  value   = "google-site-verification=o5fTXqHt2wcT1UItoGn5vLgQf8zzO4C342uNtclqhiM"
 }
